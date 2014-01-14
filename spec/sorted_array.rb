143e9e30b4029a1d4c8c2ea83540e44a79672459
gthetwong <gthetwong@gmail.com>
2014-01-14 09:57:42 -0800

    1 failure left after lab review

--- spec/sorted_array.rb
+++ spec/sorted_array.rb
@@ -2,7 +2,8 @@ class SortedArray
   attr_accessor :internal_arr # This is for convenience in the tests.
 
   def initialize(input_arr=[])
-    @internal_arr = input_arr
+    @internal_arr = []
+    input_arr.each {|x| add(x)}
     # Fill in the rest of the initialize method here.
     # What should you do with each element of the incoming array?
   end
@@ -13,11 +14,11 @@ class SortedArray
   end
 
   def size
-    @size = @internal_arr.size
+    @size = @internal_arr.length
   end
 
   def [](index)
-    @internal_arr.fetch(index, nil)
+    @internal_arr[index]
     #gets value and yields the number of the index
 
   end
@@ -25,23 +26,26 @@ class SortedArray
   def first_larger_index(target, start_ind=0, end_ind=@internal_arr.size)
     # first_larger_index should find the index of the first element in the array that's larger than the "target" passed into it
     # s/the array/the sorted array
+    
+    mid_ind = ( end_ind + start_ind )/2 
+    mid_element = @internal_arr[mid_ind]
+
     if @internal_arr.empty? 
       return 0
     elsif start_ind >= end_ind
       return end_ind
-    else 
-      mid_ind = ( end_ind + start_ind )/2 
-      if target > @internal_arr[ mid_ind ]
+    elsif target == mid_element
+      return mid_ind
+    elsif target > mid_element
         return first_larger_index(target, mid_ind + 1, end_ind)
-      else 
+    elsif target < mid_element
         return first_larger_index(target, start_ind, mid_ind)
-      end 
     end 
     # raise NotImplementedError.new("You need to implement the first_larger_index method!")
   end
 
   def index(target)
-    ind = @internal_arr.index(first_larger_index(target))
-
+    # ind = @internal_arr.index(first_larger_index(target))
+     @internal_arr.index(target)
   end
 end
